34851a67bce5b61902f42f0b806298b2dfa6957a
Codeweaver <jereco11@gmail.com>
2019-05-28 12:06:37 +0100

    Addedd Base Entity

---
+++ Vatebra.DataAccessLayer/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Vatebra.DataAccessLayer.Entities
+{
+    public class BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public DateTime dateCreated { get; set; } = DateTime.Now;
+        public DateTime dateModified { get; set; } = DateTime.Now;
+        public bool isActive { get; set; } = true;
+    }
+}
